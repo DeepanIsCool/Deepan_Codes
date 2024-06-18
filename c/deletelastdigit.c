77b45ca36da791baa0e0ccc9953b34c86bb35136
DeepanIsCool <deepan.sadhukhan2023@iem.edu.in>
2024-06-18 12:34:41 +0530

    1st Commit

---
+++ c/deletelastdigit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ 
+ int main(){
+    int n;
+    printf("Enter a num: ");
+    scanf("%d",&n);
+    int ld = n - ((n/10) * 10);
+    printf("Last Digit: %d\n",ld);
+    printf("New Numer: %d\n", (n-ld)/10 );
+    return 0;
+ }
