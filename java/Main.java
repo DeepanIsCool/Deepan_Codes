adda4a40dc0c02fb8ae091f55f90f095d9e4eb5c
DeepanIsCool <deepan.sadhukhan2023@iem.edu.in>
2025-02-25 13:49:44 +0530

    feat: Implement method overriding example with Animal and Dog classes in Java

---
+++ java/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Question 9:
+ * Write a Java program that demonstrates method overriding using a class `Animal` and
+a subclass `Dog`.
+- The `Animal` class should have a method `sound()` that prints &quot;Animal makes a
+sound&quot;.
+- The `Dog` class should override the `sound()` method to print &quot;Dog barks&quot;.
+Create objects of both classes and call the `sound()` method to demonstrate method
+overriding.
+ */
+
+class Animal {
+    // Method to print the sound of an animal
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    // Overriding the sound method to print the sound of a dog
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an object of the Animal class and call the sound method
+        Animal animal = new Animal();
+        animal.sound(); // Output: Animal makes a sound
+
+        // Create an object of the Dog class and call the sound method
+        Dog dog = new Dog();
+        dog.sound(); // Output: Dog barks
+    }
+}
